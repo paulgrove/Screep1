4a91ffb42f13f6621d31f537ad9564fa736b9b72
Paul Grove <paul.a.grove@gmail.com>
2017-03-25 15:08:27 +0000

    Create run() method on RoomController to manage per tick stages

--- RoomController.js
+++ RoomController.js
@@ -14,34 +14,47 @@ RoomController.prototype.getRoomStage = function (room) {
     if (room.find(FIND_MY_CREEPS) < 2)
         stage = 1;
     return stage;
+};
+
+RoomController.prototype.run = function () {
+	var donePlanThisTick = false;
+    for (var roomName in Game.rooms) {
+		if (!donePlanThisTick) {
+			var room = Game.rooms[roomName];
+			if (room.memory.plan_checked_tick &&
+					Game.time <= room.memory.plan_checked_tick + 30)
+				continue;
+			room.memory.plan_checked_tick = Game.time;
+			var stageNum = this.getRoomStage(room, stageNum);
+			if (room.memory.plan && room.memory.plan.stage === stageNum)
+				continue; // skip rooms which are still on the right stage
+			this.updatePlans(room);
+			donePlanThisTick = true;
+		}
+		this.recruitCreeps(room);
+	}
 }
 
-RoomController.prototype.updatePlans = function () {
-    this.updateCounter++;
-    for (var roomName in Game.rooms) {
-        var room = Game.rooms[roomName];
-		if (room.memory.plan_checked_tick && Game.time <= room.memory.plan_checked_tick + 30)
-			continue; // skip rooms checked within the last 10 ticks
-		room.memory.plan_checked_tick = Game.time;
-		var stageNum = this.getRoomStage(room);
-		if (room.memory.plan && room.memory.plan.stage === stageNum)
-			continue; // skip rooms which are still on the right stage
-		var StageModule = require("Stage" + stageNum);
-		var stage = new StageModule(room);
-		var plan = stage.compilePlan();
-		room.memory.plan = stage.compilePlan();
-		console.log(room.memory.plan);
-		console.log("Assigned new plan for room " + roomName + " Stage " + stageNum);
-		return; // only plan one room per tick
+RoomController.prototype.recruitCreeps function (room) {
+};
 
-        /*console.log("Stage: " + this.getRoomStage(room));
-        var sources = room.find(FIND_SOURCES);
-        for (var i = 0; i < sources.length; i++) {
-            var source = sources[i]function guid() {
-            console.log("Looking at source " + i + " energy: " + source.energy);
-        }
+RoomController.prototype.updatePlans = function (room, stageNum) {
+	if (room.memory.plan && room.memory.plan.stage === stageNum)
+		continue; // skip rooms which are still on the right stage
+	var StageModule = require("Stage" + stageNum);
+	var stage = new StageModule(room);
+	var plan = stage.compilePlan();
+	room.memory.plan = stage.compilePlan();
+	console.log(room.memory.plan);
+	console.log("Assigned new plan for room " + roomName + " Stage " + stageNum);
+
+	/*console.log("Stage: " + this.getRoomStage(room));
+	var sources = room.find(FIND_SOURCES);
+	for (var i = 0; i < sources.length; i++) {
+		var source = sources[i]function guid() {
+		console.log("Looking at source " + i + " energy: " + source.energy);
+	}
 		*/
-    }
 	/*
     for (var spawnName in Game.spawns) {
         var spawn = Game.spawns[spawnName];
--- main.js
+++ main.js
@@ -4,8 +4,5 @@ var roomController = new RoomController();
 
 module.exports.loop = function () {
 	//console.log(Game.time);
-	roomController.updatePlans();
-	//roomController.recruitCreeps();
-	//roomController.hireCreeps();
-	//roomController.creepActions();
+	roomController.run();
 }
